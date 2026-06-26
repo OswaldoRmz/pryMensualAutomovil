a5776531a489c12f26b470cc0db3d8dcb8947311
Diego <20251021@uthh.edu.mx>
2026-06-26 09:21:20 -0600

    se creo la clase camioneta

---
+++ clsCamioneta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryMensualAutomovil
+{
+    internal class clsCamioneta : clsTarifa
+    {
+        public double horas {  get; set; }
+
+        public override double CalularTarifa()
+        {
+            return horas * 45;
+        }
+    }
+}
