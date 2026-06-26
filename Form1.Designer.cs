61ea31a3361e81933fdf0f6bac1c5cd2f4d5e5ad
Oswaldo Aldahir Flores Ramirez <20250994@uthh.edu.mx>
2026-06-26 08:58:15 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace pryMensualAutomovil
+{
+    partial class fmrPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.btnCalcularTarifa = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIENVENIDO A ESTACIONAMIENTO BETO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Horas estacionado:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tipo de vehiculo:";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Moto",
+            "Automovil",
+            "Camioneta"});
+            this.cmbTipo.Location = new System.Drawing.Point(143, 134);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(279, 24);
+            this.cmbTipo.TabIndex = 4;
+            // 
+            // btnCalcularTarifa
+            // 
+            this.btnCalcularTarifa.Location = new System.Drawing.Point(143, 202);
+            this.btnCalcularTarifa.Name = "btnCalcularTarifa";
+            this.btnCalcularTarifa.Size = new System.Drawing.Size(168, 52);
+            this.btnCalcularTarifa.TabIndex = 5;
+            this.btnCalcularTarifa.Text = "Calcular Tarifa";
+            this.btnCalcularTarifa.UseVisualStyleBackColor = true;
+            this.btnCalcularTarifa.Click += new System.EventHandler(this.btnCalcularTarifa_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(15, 275);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(41, 16);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "Total:";
+            // 
+            // fmrPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 327);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnCalcularTarifa);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "fmrPrincipal";
+            this.Text = "Estacionamiento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Button btnCalcularTarifa;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryMensualAutomovil
+{
+    public partial class fmrPrincipal : Form
+    {
+        public fmrPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcularTarifa_Click(object sender, EventArgs e)
+        {
+            if (cmbTipoBoleto.Text == "Estudiante")
+            {
+                boleto = new clsEstudiantes(txtTipo.Text);
+            }
+            else if (cmbTipoBoleto.Text == "Publico en General")
+            {
+                boleto = new clsGeneral();
+            }
+            else if (cmbTipoBoleto.Text == "Adulto Mayor")
+            {
+                boleto = new BoletoAdultoMayor(txtTipo.Text);
+            }
+        }
+    }
+}
